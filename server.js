4cb4ddd3b9acaf1b118dae8b88b3c88e50b84b4f
Nycolas Antony Salvego <105953832+ZYONSpiral084@users.noreply.github.com>
2025-11-17 19:34:30 -0300

    Use environment variable for server port

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/reserva', (req, res) => {
 });
 
 // ==================== INICIAR SERVIDOR ====================
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
